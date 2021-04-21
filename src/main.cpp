cf22f00966c06a4133bdc328a41b2e0b14a190f9
LaserPete <pete.t.doyle@gmail.com>
2021-04-21 12:54:08 +0200

    Sketch general design

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,79 @@
 #include <Arduino.h>
 
+/*
+The general scheme will be like this
+
+Get sensor data
+
+Error check sensor data
+
+Write data to log
+
+Transmit the logged raw data to some store for possible later use eg. google
+sheets
+
+Generate a dashboard type of interface that gives useful information eg. trends,
+graphs, averages
+
+Be able to access that dashboard from mobile phone / base station / web portal
+*/
+
+void startSerial() { ; }
+void startTwoWire() { ; }
+void initializeSensors() { ; }
+void initializeCamera() { ; }
+
 void setup() {
-  // put your setup code here, to run once:
+  startSerial();
+  startTwoWire();
+  initializeSensors();
+  initializeCamera();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+// Functions that get sensor data
+int getRelativeHumidity() { ; }
+float getAirTemperature() { ; }
+int getInternalHumidity() { ; }
+int getInternalTemperature() { ; }
+int getProbeTemperature() { ; }
+int getCO2Concentration() { ; }
+int getIlluminanceVisible() { ; }
+int getIlluminanceUV() { ; }
+int getTime() { ; }
+float getBatteryVoltage() { ; }
+void takePicture() { ; }
+
+void errorCheckSensorData() { ; }
+
+void writeDataLog() { ; }
+
+// Functions that modify sensor data
+void appendEnvironmentalsToPicture() { ; }
+int averageSensorDataInt(int numberOfSamplesToAverage, int samples) { ; }
+float averageSensorDataFloat(int numberOfSamplesToAverage, float samples) { ; }
+
+void DisplayDataLogOnboardDisplay() { ; }
+
+// Sending the Data log over various channels
+void sendDataLogToGoogleSheets() { ; }
+void hostDataLogLAN() { ; }
+
+void sendDataLogToBaseStation() { ; }
+void sendDataLogOverBluetooth() { ; }
+void sendDataLogOverWiFi() { ; }
+void hostDataLogWAN() { ; }
+void sendDataLogOverGSM() { ; }
+void sendDataLogOverLoRaWAN() { ; }
+void hostDataLogUSBStorageVolume() { ; }
+void writeDataLogToSDCard() { ; }
+
+// User defined operating parameters
+void userDefineCameraParameters() { ; }
+void userSetSensorSampleRates() { ; }
+void userSetPictureDataAppend() { ; }
+
+void makeGraph() { ; }
+void userSetGraphParameters() { ; }
+void DisplayGraphOnboardDisplay() { ; }
+
+void loop() { ; }
